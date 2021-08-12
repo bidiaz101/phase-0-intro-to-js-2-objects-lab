9aaee781fed9b797f7f48bf993c5e5a4d069efc0
Brian Diaz <bidiaz101@gmail.com>
2021-08-12 17:46:00 -0500

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const employee = {
-    name: "Joe",
-    streetAddress: "123 Sesame Street"
+    name: 'Bob',
+    streetAddress: '123 Sesame Street',
 };
-function updateEmployeeWithKeyAndValue(object, key, value) {
-    return Object.assign({}, object, {[key]: value});
-}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+};
+
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
-}
+};
+
 function deleteFromEmployeeByKey(employee, key) {
     const updatedEmployee = {...employee};
     delete updatedEmployee[key];
     return updatedEmployee;
-}
+};
+
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
-    return employee
-}
+    return employee;
+};
